7c3a0a16dc13ff16c5b93454158218f3fe83d3a8
Shashikant.Kothavale <Shashikant.Kothavale@SAGITEC-1937>
2018-12-12 16:24:12 +0530

    sa

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -8,20 +8,15 @@ namespace GitTest
 {
     class Program
     {
-        //checking git version
+    /// <summary>
+    /// GIT testing
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //changes done by Bhaskar
-            //this is get test
-<<<<<<< HEAD
-            //This is my test -------------------------------
-            //This is my test -------------------------------11111111111111
-            //alkdsjflkasjflksajflkajlkfjd
-            //lajdslfkjdsaflkjdsalfkjdsaflkjdsaf
-=======
-            //merge test - 
+    
+    
 
->>>>>>> 9f071b013d92993eaefbceb0a5e99d11986fe1c7
         }
     }
 }
